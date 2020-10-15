1b2fb82c7d4dc54f23c7c06f8c57370fafa552ac
dengwx11 <wenxuan.deng@yale.edu>
2020-10-15 17:22:37 -0400

    update

--- main.R
+++ main.R
@@ -25,6 +25,11 @@ rst<-run(X,1,0,1,AS,A0,D,K,N)
 plot(as.vector(W),as.vector(rst$W))
 plot(as.vector(true.H),as.vector(rst$H))
 
+## without any penalization
+rst<-run(X,0,0,1,AS,A0,D,K,N)
+plot(as.vector(W),as.vector(rst$W))
+plot(as.vector(true.H),as.vector(rst$H))
+
 
 
 ## True postive rate
