487a17d738fbd349375cd31bcd4293276c229ff2
Biqing <biqing.zhu@yale.edu>
2020-12-31 12:27:56 -0500

    updated_plots

--- benchmarking.R
+++ benchmarking.R
@@ -12,6 +12,7 @@ celltype_pred_fact <- as.factor(celltype_pred_woa0as)
 celltype_pred_fact <- factor(celltype_pred_fact, levels = c("1", "2", "3", "4", "5"))
 pred_onehot_woa0as <- as.data.frame(t(one_hot(as.data.table(celltype_pred_fact))))
 pred_onehot_louvain <- as.data.frame(t(one_hot(as.data.table(seur$seurat_clusters))))
+pred_onehot_nnls <- as.data.frame(t(one_hot(as.data.table(as.factor(celltype_pred_nnls)))))
 
 ## Cosine similarity
 cos_sim <- mean(mapply(cosine, truth_onehot, as.data.frame(rst$H)))
@@ -19,6 +20,7 @@ cos_sim_woas <- mean(mapply(cosine, truth_onehot, as.data.frame(rst.woas$H)))
 cos_sim_woa0 <- mean(mapply(cosine, truth_onehot, as.data.frame(rst.woa0$H)))
 cos_sim_woa0as <- mean(mapply(cosine, truth_onehot, as.data.frame(rst.woa0as$H)))
 cos_sim_louvain <- mean(mapply(cosine, truth_onehot, pred_onehot_louvain))
+cos_sim_nnls <- mean(mapply(cosine, truth_onehot, pred_onehot_nnls))
 
 ## NMI
 nmi <- mean(mapply(NMI, truth_onehot, as.data.frame(pred_onehot)))
@@ -26,6 +28,7 @@ nmi_woas <- mean(mapply(NMI, truth_onehot, as.data.frame(pred_onehot_woas)))
 nmi_woa0 <- mean(mapply(NMI, truth_onehot, as.data.frame(pred_onehot_woa0)))
 nmi_woa0as <- mean(mapply(NMI, truth_onehot, as.data.frame(pred_onehot_woa0as)))
 nmi_louvain<- mean(mapply(NMI, truth_onehot, as.data.frame(pred_onehot_louvain)))
+nmi_nnls<- mean(mapply(NMI, truth_onehot, as.data.frame(pred_onehot_nnls)))
 
 ## ARI
 ari <- adjustedRandIndex(celltype_pred, truth)
@@ -33,6 +36,7 @@ ari_woas <- adjustedRandIndex(celltype_pred_woas, truth)
 ari_woa0 <- adjustedRandIndex(celltype_pred_woa0, truth)
 ari_woa0as <- adjustedRandIndex(celltype_pred_woa0as, truth)
 ari_louvain <- adjustedRandIndex(as.numeric(seur$seurat_clusters), truth)
+ari_nnls <- adjustedRandIndex(celltype_pred_nnls, truth)
 
 ## Accuracy
 accu <- cnt_max / N
@@ -40,6 +44,7 @@ accu_woas <- cnt_max_woas / N
 accu_woa0 <- cnt_max_woa0 / N
 accu_woa0as <- cnt_max_woa0as / N
 accu_louvain <- mean(pred_onehot_louvain == truth)
+accu_nnls <- cnt_max_nnls / N
 
 ## Silhouette
 pca.data <- list()
@@ -48,22 +53,37 @@ sil <- batch_sil(pca.data, celltype_pred)
 sil_woas <- batch_sil(pca.data, celltype_pred_woas)
 sil_woa0 <- batch_sil(pca.data, celltype_pred_woa0)
 sil_woa0as <- batch_sil(pca.data, celltype_pred_woa0as)
-sil_louvain <- batch_sil(pca.data, celltype_pred_woa0as)
+sil_louvain <- batch_sil(pca.data, as.numeric(seur$seurat_clusters))
+sil_nnls <- batch_sil(pca.data, celltype_pred_nnls)
+
+# pars <- data.frame(
+#     names = rep(c('Accuracy', 'ARI', 'Cosine Similarity', 'NMI', 'ASW'), 5),
+#     vars = c(accu, ari, cos_sim, nmi, sil, 
+#              accu_woas, ari_woas, cos_sim_woas, nmi_woas, sil_woas,
+#              accu_woa0, ari_woa0, cos_sim_woa0, nmi_woa0, sil_woa0,
+#              accu_woa0as, ari_woa0as, cos_sim_woa0as, nmi_woa0as, sil_woa0as,
+#              accu_louvain, ari_louvain, cos_sim_louvain, nmi_louvain, sil_louvain),
+#     group = c(rep('Full', 5), rep('w/o AS', 5), rep('w/o A0', 5), rep('w/o A0 AS', 5), rep('Louvain', 5))
+# )
 
 pars <- data.frame(
-    names = rep(c('Accuracy', 'ARI', 'Cosine Similarity', 'NMI', 'ASW'), 5),
+    models = c(rep('Full', 5), rep('w/o AS', 5), rep('w/o A0', 5), rep('w/o A0 AS', 5), rep('Louvain', 5), rep('NNLS', 5)),
     vars = c(accu, ari, cos_sim, nmi, sil, 
              accu_woas, ari_woas, cos_sim_woas, nmi_woas, sil_woas,
              accu_woa0, ari_woa0, cos_sim_woa0, nmi_woa0, sil_woa0,
              accu_woa0as, ari_woa0as, cos_sim_woa0as, nmi_woa0as, sil_woa0as,
-             accu_louvain, ari_louvain, cos_sim_louvain, nmi_louvain, sil_louvain),
-    group = c(rep('Full', 5), rep('w/o AS', 5), rep('w/o A0', 5), rep('w/o A0 AS', 5), rep('Louvain', 5))
+             accu_louvain, ari_louvain, cos_sim_louvain, nmi_louvain, sil_louvain,
+             accu_nnls, ari_nnls, cos_sim_nnls, nmi_nnls, sil_nnls),
+    metric = rep(c('Accuracy', 'ARI', 'Cosine Similarity', 'NMI', 'ASW'), 6)
 )
-p <- ggplot(data = pars, aes(x = names, y = vars, fill = group)) +
+pars$models <- factor(pars$models,levels = c("NNLS", "Louvain", "w/o A0 AS", "w/o A0", "w/o AS", "Full"))
+pars$metric <- factor(pars$metric,levels = c('ASW', 'NMI', 'Cosine Similarity', 'ARI', 'Accuracy'))
+
+p <- ggplot(data = pars, aes(x = models, y = vars, fill = metric, width=.7)) +
   geom_bar(stat="identity", position=position_dodge()) +
-  ggtitle('Senario 1') + 
+  labs(y = 'Values', title = 'Senario 1') +
   coord_flip()
-ggsave('/Users/mac/Desktop/Yale/Hongyu/CyTOF/senario1_benchmark.png', p)
+ggsave('/Users/mac/Desktop/Yale/Hongyu/CyTOF/senario1_benchmark_v2.png', p)
 
 
 
--- w_corr.R
+++ w_corr.R
@@ -2,52 +2,98 @@ source("Simulation.R")
 source('run_para.R')
 library(ggplot2)
 
-loops <- 100
-corr_vec <- c()
-i <- 0
-while(i < loops) {
-    ## generate dataset: X, AS, A0, W(ground truth), true.label
-    gamma <- simulate_gamma(iteration, K, D)
-    A0 <- gamma$gamma
-    W <- simulate_w(D,K,A0, p.0 = p.0, q.0=q.0, p.neg1= p.neg1, q.neg1 = q.neg1, 
-                big_w_mean=big_w_mean, big_tau_w = big_tau_w, 
-                small_w_mean=small_w_mean, small_tau_w = small_tau_w)
-    AS <- simulate_AS(D,K,W,corr=corr)
-    label.output <- simulate_label(D,N,K,prob_k)
-    X <- simulate_x(D,N,W,label.output$label, mean_var_ratio = mean_var_ratio)
-    true.H <- label.output$H
+# loops <- 100
+# corr_vec <- c()
+# i <- 0
+# while(i < loops) {
+#     ## generate dataset: X, AS, A0, W(ground truth), true.label
+#     gamma <- simulate_gamma(iteration, K, D)
+#     A0 <- gamma$gamma
+#     W <- simulate_w(D,K,A0, p.0 = p.0, q.0=q.0, p.neg1= p.neg1, q.neg1 = q.neg1, 
+#                 big_w_mean=big_w_mean, big_tau_w = big_tau_w, 
+#                 small_w_mean=small_w_mean, small_tau_w = small_tau_w)
+#     AS <- simulate_AS(D,K,W,corr=corr)
+#     label.output <- simulate_label(D,N,K,prob_k)
+#     X <- simulate_x(D,N,W,label.output$label, mean_var_ratio = mean_var_ratio)
+#     true.H <- label.output$H
 
-    rownames(X) <- c(1:D)
-    colnames(X) <- c(1:N)
-    seur <- CreateSeuratObject(X)
-    seur@meta.data$label <- label.output$label[1,]
-    seur <- NormalizeData(seur) 
-    seur@assays$RNA@data = seur@assays$RNA@counts
-    seur <- FindVariableFeatures(seur, slot='counts', selection.method = "vst", nfeatures = 10)
-    seur <- ScaleData(seur)
-    seur <- RunPCA(seur,verbose = TRUE,features = rownames(seur), seed.use = NULL)
-    seur <- FindNeighbors(seur, dims = 1:6)
-    seur <- FindClusters(seur, resolution = .6)
+#     rownames(X) <- c(1:D)
+#     colnames(X) <- c(1:N)
+#     seur <- CreateSeuratObject(X)
+#     seur@meta.data$label <- label.output$label[1,]
+#     seur <- NormalizeData(seur) 
+#     seur@assays$RNA@data = seur@assays$RNA@counts
+#     seur <- FindVariableFeatures(seur, slot='counts', selection.method = "vst", nfeatures = 10)
+#     seur <- ScaleData(seur)
+#     seur <- RunPCA(seur,verbose = TRUE,features = rownames(seur), seed.use = NULL)
+#     seur <- FindNeighbors(seur, dims = 1:6)
+#     seur <- FindClusters(seur, resolution = .6)
 
-    rst.para<-runOptimalPara(X,AS,A0,D,K,N, epsilon = 0.01,fixed_loop=50,depth=2,lambda1.on=T,lambda2.on=T)
-    rst<-run(X,rst.para$para$lambda1,rst.para$para$lambda2,rst.para$para$mu,rst.para$para$eta,
-            AS,A0,D,K,N, epsilon = 10^(-3),fixed_loop=2000)
-    corr_vec <- c(corr_vec, cor(as.vector(rst$W), as.vector(W)))
-    print(corr_vec)
-    print(i)
-    i <- i + 1
+#     rst.para<-runOptimalPara(X,AS,A0,D,K,N, epsilon = 0.01,fixed_loop=50,depth=2,lambda1.on=T,lambda2.on=T)
+#     rst<-run(X,rst.para$para$lambda1,rst.para$para$lambda2,rst.para$para$mu,rst.para$para$eta,
+#             AS,A0,D,K,N, epsilon = 10^(-3),fixed_loop=2000)
+#     corr_vec <- c(corr_vec, cor(as.vector(rst$W), as.vector(W)))
+#     print(corr_vec)
+#     print(i)
+#     i <- i + 1
+# }
+
+# write.table(corr_vec, '/Users/mac/Desktop/Yale/Hongyu/CyTOF/w_corr_vec.txt')
+
+run_eachW <- function(X,lambda1,lambda2,mu, eta, AS, A0, D, K, N, epsilon = 10^(-4),fixed_loop = 0) {
+    ## Initialization
+    W_iter <- matrix(1,nrow = D, ncol = K)
+    H <- matrix(1,nrow = K, ncol = N)
+    corr_vec <- c()
+
+    L.current <- compute_L(X,W,H,lambda1,lambda2,mu,eta)
+    L.min <- L.current
+    L.prev <- 100000
+    L.save <- c(L.current)
+    k=1
+     while(abs(L.prev - L.current) > epsilon){
+    #while(abs(L.prev - L.current) > epsilon && L.min >= L.current){
+    #for(i in 1:1000){
+        W_iter <- update_W(X, W_iter, H, mu, AS, A0, D, K, lambda1, lambda2)
+        corr_vec <- c(corr_vec, cor(as.vector(W_iter), as.vector(W)))
+        L.prev <- L.current
+        L.current <- compute_L(X,W_iter,H,lambda1,lambda2,mu,eta)
+        #print(paste0("update W, L change ", L.current-L.prev))
+        H <- update_H(X, W_iter, H, K, N, eta)
+        L.prev <- L.current
+        L.current <- compute_L(X,W_iter,H,lambda1,lambda2,mu,eta)
+        #print(paste0("update H, L change ", L.current-L.prev))
+        L.save <- c(L.save, L.current)
+        if(L.min>L.current) L.min=L.current
+        #print(L.current)
+        k = k+1
+        if(fixed_loop >0 && k>fixed_loop) break
+    }
+
+    print(paste0("iteration = ",k))
+    return(corr_vec)
 }
+corr_vec<-run_eachW(X,rst.para$para$lambda1,rst.para$para$lambda2,rst.para$para$mu,rst.para$para$eta,
+            AS,A0,D,K,N, epsilon = 10^(-3),fixed_loop=2000)
 
-write.table(corr_vec, '/Users/mac/Desktop/Yale/Hongyu/CyTOF/w_corr_vec.txt')
-
-png("/Users/mac/Desktop/Yale/Hongyu/CyTOF/senario1_w_corr.png") 
-plot(corr_vec, type = 'l', xlab = 'Simulation', ylab = 'Correlation', main = 'Correlation between W and W^hat (Senario 1)')
-dev.off() 
+p_corr <- ggplot(mapping = aes(x = 1:300, y = corr_vec[1:300])) +
+geom_line() +
+labs(x = 'Epoch', y = 'Correlation', title = 'Correlation between W and W^hat (Senario 1)')
+ggsave("/Users/mac/Desktop/Yale/Hongyu/CyTOF/senario1_w_corr_v1.png", p_corr)
+# png("/Users/mac/Desktop/Yale/Hongyu/CyTOF/senario1_w_corr.png") 
+# plot(corr_vec, type = 'l', xlab = 'Simulation', ylab = 'Correlation', main = 'Correlation between W and W^hat (Senario 1)')
+# dev.off() 
 
 ## Loss
-png("/Users/mac/Desktop/Yale/Hongyu/CyTOF/senario1_log_loss.png")
-plot(rst$L.save, log="y", xlab = 'Epoch', ylab = 'Log loss', main = 'Loss (Senario 1 log scale)')
+png("/Users/mac/Desktop/Yale/Hongyu/CyTOF/senario1_log_loss_v1.png")
+plot(rst$L.save[1:100], log="y", xlab = 'Epoch', ylab = 'Log loss', main = 'Loss (Senario 1 log scale)')
 dev.off() 
+
+p <- ggplot(mapping = aes(y = rst$L.save[1:100], x = 1:100)) + 
+geom_point() +
+scale_y_continuous(trans='log2') +
+labs(title="Log Loss (Senario 1)", x ="Epoch", y = "Log loss")
+ggsave('/Users/mac/Desktop/Yale/Hongyu/CyTOF/senario1_log_loss_v2.png', p)
 
 png("/Users/mac/Desktop/Yale/Hongyu/CyTOF/senario1_loss.png")
 plot(rst$L.save, xlab = 'Epoch', ylab = 'Loss', main = 'Loss (Senario 1)')
