a733880f91723fe7005429b001521252bcd4fc01
Biqing <biqing.zhu@yale.edu>
2021-01-23 16:42:28 -0500

    Update subsetting_metrics.R

--- BCR_RealApplication/subsetting_metrics.R
+++ BCR_RealApplication/subsetting_metrics.R
@@ -27,14 +27,13 @@ H_est_subsetting <- apply(H_est_subsetting,2,function(x) x/sum(x))
 
 cutoff.list <- c(1:100)/100                          
 df <- data.frame(cutoff = cutoff.list)
-df$acc <- df$N <- df$cos <- df$nmi <- df$ari <- df$sil <- 0   
-df$cutoff <- cutoff.list                    
+df$acc <- df$N <- df$cos <- df$nmi <- df$ari <- df$sil <- 0              
                           
 for(j in c(1:length(cutoff.list))){
     ## cnt_max
     cnt_max = 0
     N_subsetting = 0
-    idx.subsetting = 0
+    idx.subsetting = c()
         for(i in 1:length(truth)){
             if(max(H_est_subsetting[,i])>cutoff.list[j]){
                 idx.subsetting = c(idx.subsetting,i)
@@ -48,38 +47,37 @@ for(j in c(1:length(cutoff.list))){
     } else {
        df$acc[j]<-cnt_max/N_subsetting
     } 
-    print(length(idx.subsetting))
-
-    df$N[j] <- N_subsetting
+    if(length(idx.subsetting) > 0) {
+        df$N[j] <- N_subsetting
     
-    truth_sub <- truth[idx.subsetting]
-    truth_fact_sub <- as.factor(truth_sub)
-    # truth_fact_sub <- factor(truth_fact_sub, levels = as.character(1:nrow(H_est_subsetting)))
+        truth_sub <- truth[idx.subsetting]
+        truth_fact_sub <- as.factor(truth_sub)
+        # truth_fact_sub <- factor(truth_fact_sub, levels = as.character(1:nrow(H_est_subsetting)))
 
-    
-    celltype_pred_sub <- seur$pred[idx.subsetting]
-    celltype_pred_fact_sub <- as.factor(celltype_pred_sub)
-    celltype_pred_fact_sub <- factor(celltype_pred_fact_sub, levels = levels(truth_fact_sub))
+        
+        celltype_pred_sub <- seur$pred[idx.subsetting]
+        celltype_pred_fact_sub <- as.factor(celltype_pred_sub)
+        celltype_pred_fact_sub <- factor(celltype_pred_fact_sub, levels = levels(truth_fact_sub))
 
-    ## ARI
-    ari <- adjustedRandIndex(celltype_pred_sub, truth_sub)
-    df$ari[j] <- ari
-    
-    ## Cosine similarity
-    print(head(truth_fact_sub))
-    truth_onehot_sub <- as.data.frame(t(one_hot(as.data.table(truth_fact_sub))))
-    cos_sim <- mean(mapply(cosine, truth_onehot_sub, as.data.frame(H_est_subsetting[,idx.subsetting])))
-    df$cos[j] <- cos_sim
+        ## ARI
+        ari <- adjustedRandIndex(celltype_pred_sub, truth_sub)
+        df$ari[j] <- ari
+        
+        ## Cosine similarity
+        truth_onehot_sub <- as.data.frame(t(one_hot(as.data.table(truth_fact_sub))))
+        cos_sim <- mean(mapply(cosine, truth_onehot_sub, as.data.frame(H_est_subsetting[,idx.subsetting])))
+        df$cos[j] <- cos_sim
 
-    ## NMI
-    pred_onehot_sub <- as.data.frame(t(one_hot(as.data.table(celltype_pred_fact_sub))))
-    nmi <- mean(mapply(NMI, truth_onehot_sub, as.data.frame(pred_onehot_sub)))
-    df$nmi[j] <- nmi
+        ## NMI
+        pred_onehot_sub <- as.data.frame(t(one_hot(as.data.table(celltype_pred_fact_sub))))
+        nmi <- mean(mapply(NMI, truth_onehot_sub, as.data.frame(pred_onehot_sub)))
+        df$nmi[j] <- nmi
 
-    ## Silhouette
-    pca.data <- list()
-    pca.data$x <- seur@reductions$pca@cell.embeddings[idx.subsetting,]
-    sil <- batch_sil(pca.data, as.numeric(as.factor(celltype_pred_sub)))
-    df$sil[j] <- sil
+        ## Silhouette
+        pca.data <- list()
+        pca.data$x <- seur@reductions$pca@cell.embeddings[idx.subsetting,]
+        sil <- batch_sil(pca.data, as.numeric(as.factor(celltype_pred_sub)))
+        df$sil[j] <- sil
+    }
 }
 write.table(df, paste0('/home/bz234/project/Results/CyTOF/BCR_Cytof/subset_metrics/', sample, '.txt'))
